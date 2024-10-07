1f5d1272f1b8d3845f48fdcda0836fece0ecde1c
Rangga Mukti <r4n994mukti@gmail.com>
2024-10-07 11:35:16 +0700

    Menambahkan Fitur Search

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import defaultImage from '../../assets/pokemon-not-found.png';
+
 import pokemonJSON from '../../data/pokemon.json';
 import PokemonItem from '../PokemonItem/PokemonItem';
 
@@ -7,13 +9,31 @@ import './PokemonList.css';
 
 const PokemonList = () => {
   const [pokemons] = useState(pokemonJSON);
+  const [filterPokemons, setFilterPokemons] = useState(pokemonJSON);
+
+  const handleSearch = (event) => {
+    let search = pokemons.filter((item) => {
+      return item.name.toLowerCase().includes(event.target.value);
+    });
+
+    setFilterPokemons(search);
+  };
 
   return (
     <div>
+      <input type="text" placeholder="cari pokemon kamu..." className="search" onChange={handleSearch} />
       <div className="list-pokemon">
-        {pokemons.map((item) => (
+        {/* {pokemons.map((item) => (
           <PokemonItem key={item.id} pokemon={item} />
-        ))}
+        ))} */}
+
+        {filterPokemons.length == 0 ? (
+          <div>
+            <img src={defaultImage} alt="Data Tidak Ditemukan" width={350} />
+          </div>
+        ) : (
+          filterPokemons.map((item) => <PokemonItem key={item.id} pokemon={item} />)
+        )}
       </div>
     </div>
   );
